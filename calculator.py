8217094fe085d62d609217d9ae8dc14a91390a88
Deemple <deemplebhatt@gmail.com>
2026-06-17 20:49:06 +0530

    Add calculator

---
+++ calculator.py
@@ -0,0 +1,12 @@
+print("Search operation:")
+a=2
+b=3
+c=7
+print("Addition=", a+b+c)
+print("Subtraction=", a-b-c)
+print("Multiplication=", a*b*c)
+print("Division=", a/b/c)
+print("Module=", a%b%c)
+print("average",a,b,c)
+
+
